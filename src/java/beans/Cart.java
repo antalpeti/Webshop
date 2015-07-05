3350b09046bd8e6578d07ddcbd8871558ea4071b
Peti <antalpeti@users.noreply.github.com>
2015-07-05 17:42:03 +0200

    Find user from the database according to the given username and password. Navigate to the cart page if the user exists.

--- src/java/beans/Cart.java
+++ src/java/beans/Cart.java
@@ -11,6 +11,8 @@ import java.util.HashSet;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import pojos.Client;
 import pojos.Order;
 import pojos.Orderitem;
@@ -27,6 +29,8 @@ public class Cart {
     private List<Orderitem> orderList;
     private Order order;
     private Client client;
+    private String username;
+    private String password;
 
     /**
      * Creates a new instance of Basket
@@ -36,22 +40,46 @@ public class Cart {
         order = new Order(client, false, new Date(), new HashSet<Orderitem>());
     }
 
+    public String findClient() {
+        if (username != null || password != null) {
+            Session session = hibernate.HibernateUtil.getSessionFactory().openSession();
+            List<Client> clients;
+            Query query = session.createQuery("FROM Client WHERE username =:username AND password =:password");
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+            clients = query.list();
+            if (clients.size() > 0) {
+                client = clients.get(0);
+            }
+            session.close();
+        }
+        if (client != null) {
+            return "login2cart";
+        } else {
+            return "login2login";
+        }
+    }
+
+    public void registerClient(Client client) {
+
+    }
+
     public String put(Product product) {
         Orderitem oi = new Orderitem(order, product, 1, product.getPrice());
         orderList.add(oi);
-        
+
         return "index2cart";
     }
-    
-    public void remove(Orderitem oi){
+
+    public void remove(Orderitem oi) {
         orderList.remove(oi);
     }
-    
-    public void incAmount(Orderitem oi){
+
+    public void incAmount(Orderitem oi) {
         oi.setQty(oi.getQty() + 1);
         oi.setPrice(oi.getQty() * oi.getProduct().getPrice());
     }
-    
+
     public void decAmount(Orderitem oi) {
         if (oi.getQty() > 1) {
             oi.setQty(oi.getQty() - 1);
@@ -60,15 +88,15 @@ public class Cart {
             remove(oi);
         }
     }
-    
-    public String purchase(){
+
+    public String purchase() {
         if (client == null) {
             return "cart2logreg";
         }
         return "";
     }
-    
-    public double getSumTotal(){
+
+    public double getSumTotal() {
         double sumTotal = 0;
         for (Orderitem oi : orderList) {
             sumTotal += oi.getPrice();
@@ -100,5 +128,20 @@ public class Cart {
         this.client = client;
     }
 
-    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
